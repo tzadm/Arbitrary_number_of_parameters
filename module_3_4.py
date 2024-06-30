906efaa01037c4d2a2a61e7abaa795fda220bf79
tzadm <94993624+tzadm@users.noreply.github.com>
2024-06-30 22:07:09 +0300

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,33 @@
+def single_root_words(root_world, *other_words):
+    same_words = []
+    for i in other_words:
+        if root_world in i:
+            same_words.append(i)
+        elif root_world not in i:
+            root_world = root_world.lower()
+            if root_world in i:
+                same_words.append(i)
+            elif root_world not in i:
+                root_world = str(root_world[0]).upper() + str(root_world[1:])
+                if root_world in i:
+                    same_words.append(i)
+
+    for i in other_words:
+        if i in root_world:
+            same_words.append(i)
+        elif i not in root_world:
+            i = str(i).lower()
+            if i in root_world:
+                same_words.append(i.title())
+            elif i not in root_world:
+                root_world = str(root_world).lower()
+                if i in root_world:
+                    same_words.append(i.title())
+
+    return same_words
+
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
